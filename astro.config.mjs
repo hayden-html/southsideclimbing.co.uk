f5fe5097950c5d07f0453ba9fa87c8d0134376a0
hayden-110 <99691272+hayden-110@users.noreply.github.com>
2023-02-03 19:35:26 +0000

    Added about page + insta-widget, removed bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,8 @@ import image from "@astrojs/image";
 // https://astro.build/config
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), image()]
